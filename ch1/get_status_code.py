3ee0cf29555585e165a4cbd6153786932ae0f393
mengchen0128 <1458443939@qq.com>
2022-03-06 16:07:47 +0800

    删除
    删除

--- ch1/get_status_code.py
+++
@@ -1,7 +0,0 @@
-
-
-import requests
-
-url = "http://www.httpbin.org/#/HTTP_Methods/get_get"
-r=requests.get(url)
-print(r.status_code)
--- ch1/no_args_get.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url="https://www.baidu.com"
-r=requests.get(url=url)
-print(r.url)
-print(r.status_code)
